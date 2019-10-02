cdc0630871f147fe5bece15594c5411df0a99b42
XuZhang <xuzhang.p@foxmail.com>
2019-10-02 11:45:19 +0800

    Update siamese_NN.py

--- siamese_NN.py
+++ siamese_NN.py
@@ -3,11 +3,10 @@ import os
 import tensorflow as tf
 import keras.backend.tensorflow_backend as KTF
 
-# 指定第一块GPU可用 
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 config = tf.ConfigProto()  
-config.gpu_options.allow_growth=True   #不全部占满显存, 按需分配
+config.gpu_options.allow_growth=True   
 sess = tf.Session(config=config)
 
 KTF.set_session(sess)
